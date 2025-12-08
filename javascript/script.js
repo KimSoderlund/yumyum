2d82192454905b6f90509a5ae0197b74fbe2659c
Kim Söderlund <kimsoderlund33@gmail.com>
2025-12-08 16:30:31 +0100

    Added so that cart hides virst menu and shows cart.

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,25 @@
-console.log('Funkar JS?')
+const cartBtn = document.getElementById('cartBtn');
+const menuView = document.getElementById('menuView')
 
-const url = "https://fdnzawlcf6.execute-api.eu-north-1.amazonaws.com/";
+cartBtn.addEventListener('click', ()  => {
+    menuView.classList.add('invisible');
+    menuView.classList.remove('visible');
+    cartView.classList.add('visible');
+});
 
-async function getApiKey() {
-    console.log ("funktionen startat?")
-    try {
-        const settings = {method: 'GET'};
-        const keyUrl = url+'/key';
-        console.log (keyUrl);
-        const response = await fetch(keyUrl, settings)
-        const data = await response.json();
-        console.log(data);
-    } catch (error) { console.log(error)}
-    };
+
+// console.log('Funkar JS?')
+
+// const url = "https://fdnzawlcf6.execute-api.eu-north-1.amazonaws.com/";
+
+// async function getApiKey() {
+//     console.log ("funktionen startat?")
+//     try {
+//         const settings = {method: 'GET'};
+//         const keyUrl = url+'/key';
+//         console.log (keyUrl);
+//         const response = await fetch(keyUrl, settings)
+//         const data = await response.json();
+//         console.log(data);
+//     } catch (error) { console.log(error)}
+//     };

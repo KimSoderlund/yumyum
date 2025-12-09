fde4dd6b4499b41a3bbaf398927cf01436465956
Kim Söderlund <kimsoderlund33@gmail.com>
2025-12-09 16:39:55 +0100

    Added JS for getting API key & tenant

---
+++ javascript/cartBtn.js
@@ -0,0 +1,21 @@
+const cartBtn = document.getElementById('cartBtn');
+const menuView = document.getElementById('menuView');
+const cartView = document.getElementById('cartView');
+const backgroundSelector = document.querySelector('main');
+
+
+//JS to replace views and change colors depending on view
+cartBtn.addEventListener('click', ()  => {
+    if(menuView.classList.contains('visible')){
+        menuView.classList.replace('visible', 'invisible');
+        cartView.classList.replace('invisible', 'visible')         
+        backgroundSelector.classList.replace('menu-colors','cart-colors')
+
+    } else if (cartView.classList.contains('visible')) {
+        cartView.classList.replace('visible','invisible');
+        menuView.classList.replace('invisible','visible');
+        backgroundSelector.classList.replace('cart-colors','menu-colors')
+
+    } else {console.log("Inget korrekt utfall")}
+
+});
--- javascript/script.js
+++ javascript/script.js
@@ -1,43 +1,41 @@
-const cartBtn = document.getElementById('cartBtn');
-const menuView = document.getElementById('menuView');
-const cartView = document.getElementById('cartView');
-const backgroundSelector = document.querySelector('main');
+import './cartBtn.js'
+
+const url = "https://fdnzawlcf6.execute-api.eu-north-1.amazonaws.com/";
+let apiKey = '';
+
+async function getApiKey() {
+    try {
+        const settings = {method: 'POST'};
+        const keyUrl = url+'keys';
+        const response = await fetch(keyUrl, settings)
+        const data = await response.json();
+        apiKey = data.key;
+    } catch (error) { console.log(error)}
+    };
+
+    await getApiKey();
 
 
-// TODO: Ta bort klasserna visible/invisible och ändra style istället.
-cartBtn.addEventListener('click', ()  => {
-    if(menuView.classList.contains('visible')){
-        menuView.classList.replace('visible', 'invisible');
-        cartView.classList.replace('invisible', 'visible') 
-        
-        //Change Color palette
-        backgroundSelector.classList.replace('menu-colors','cart-colors')
-        console.log("tog bort view från MenuView och bytte färger")
+    // TODO: Ta bort console.logs
+    async function getTenant() {
+        const tenantUrl = url+'tenants'
+        const bodyToSend = {name : JSON.stringify(Date.now())};
+        console.log(`name: ${JSON.stringify(bodyToSend)}`)
+        console.log(`API Key: ${apiKey}`)
+        const settings = {
+            'method': 'POST',
+            body: JSON.stringify(bodyToSend),
+            headers : {
+            "Content-Type": "application/json",
+            "x-zocom": apiKey
+            }
+        }
+        console.log(settings)
+        console.log(tenantUrl)
+        const response = await fetch(tenantUrl, settings);
+        const data = await response.json();
+        const tenant = data;
+        console.log(`New Tenant: ${tenant}`)
+    };
 
-    } else if (cartView.classList.contains('visible')) {
-        cartView.classList.replace('visible','invisible');
-        menuView.classList.replace('invisible','visible');
-        console.log("tog bort view från cartView");
-
-        backgroundSelector.classList.replace('cart-colors','menu-colors')
-
-    } else {console.log("Inget korrekt utfall")}
-
-});
-
-
-// console.log('Funkar JS?')
-
-// const url = "https://fdnzawlcf6.execute-api.eu-north-1.amazonaws.com/";
-
-// async function getApiKey() {
-//     console.log ("funktionen startat?")
-//     try {
-//         const settings = {method: 'GET'};
-//         const keyUrl = url+'/key';
-//         console.log (keyUrl);
-//         const response = await fetch(keyUrl, settings)
-//         const data = await response.json();
-//         console.log(data);
-//     } catch (error) { console.log(error)}
-//     };
+    await getTenant();

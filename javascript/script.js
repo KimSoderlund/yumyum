5b6b46afac9e1385692b54a7fb887db27f21a72f
Kim Söderlund <kimsoderlund33@gmail.com>
2025-12-10 11:32:13 +0100

    Changed HTML after David hit me.

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,3 @@
-import './state.js'
 import './cartBtn.js'
 import './API/apiInit.js'
 import './DOM.js'
--- javascript/state.js
+++
@@ -1,2 +0,0 @@
-//TO DO: vet inte vad jag ska göra här just nu. men känns som det kan va relevant
-import { apiKey, tenant } from './API/apiInit.js';

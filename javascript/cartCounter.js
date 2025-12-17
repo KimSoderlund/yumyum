29abf41791486d70e32557a37d5267d38b9876c7
Kim Söderlund <kimsoderlund33@gmail.com>
2025-12-17 08:50:38 +0100

    Added some more animations

--- javascript/cartCounter.js
+++ javascript/cartCounter.js
@@ -6,5 +6,6 @@ import { orderBtnActivation } from './sendOrderBtn.js';
 export function increaseCartCounter (cartItems) {
     let cartSize = cartItems.reduce((total, item) => total + item.quantity, 0);
     cartCounter.innerText = cartSize;
+
     orderBtnActivation();
 }
